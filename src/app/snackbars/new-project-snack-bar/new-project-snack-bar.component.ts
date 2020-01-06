f5da6a3db59d89ca4d2c79ca6695ed452687266b
Fülöp Walter <fulopw@sonrisa.hu>
2020-01-06 11:56:14 +0100

    Snackbars added

---
+++ src/app/snackbars/new-project-snack-bar/new-project-snack-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-project-snack-bar',
+  templateUrl: './new-project-snack-bar.component.html',
+  styleUrls: ['./new-project-snack-bar.component.css']
+})
+export class NewProjectSnackBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/snackbars/task-updated-snackbar/task-updated-snackbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-updated-snackbar',
+  templateUrl: './task-updated-snackbar.component.html',
+  styleUrls: ['./task-updated-snackbar.component.css']
+})
+export class TaskUpdatedSnackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
